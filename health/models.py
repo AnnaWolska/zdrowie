57a5efb37e01228c896ff777c33ebbc17e4075c9
AnnaWolska <a_wl@wp.pl>
2022-04-26 14:25:16 +0200

    firtst commit

---
+++ health/models.py
@@ -0,0 +1,20 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+
+class bloodpressure(models.Model):
+    user = models.ForeignKey("auth.User", on_delete=models.CASCADE, related_name="bloodpressures")
+    date = models.DateTimeField(auto_now_add=True)
+    result1 = models.IntegerField(null=True, blank=True)
+    result2 = models.IntegerField(null=True, blank=True)
+    comment = models.TextField(null=True, blank=True)
+
+    def __str__(self):
+        return f"{self.user} ({self.date} {self.result1} {self.result2} {self.comment})"
+
+    class Meta:
+        verbose_name = "ciśnienie"
+        verbose_name_plural = "ciśnienia"
+
+
+class glucoses(models.Model):
